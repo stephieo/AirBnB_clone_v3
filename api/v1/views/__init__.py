0b20870eaf9f5b5498499875ad8a398e606fa850
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-04-02 09:54:38 +0100

    fix : import error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from flask import Blueprint
-from api.v1.views.index import *
 """ holds the blueprints"""
 
 app_views = Blueprint("views", __name__, url_prefix='/api/v1')
 
+from api.v1.views.index import *
 
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
-""" index file in API """
+""" index file , returns a JSON response """
 
-app = Flask(__name__)
+from api.v1.views import app_views
+
 
 @app_views.route("/status")
 def status():
